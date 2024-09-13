a9bc4ec7534bd5f40b2bd937bcd3ed329c08a18d
sidHarsh34 <140314434+sidHarsh34@users.noreply.github.com>
2024-09-14 00:37:14 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+mongoose.connect("mongodb://localhost:27017").then(() =>{
+    console.log('Connect to mongodb');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
