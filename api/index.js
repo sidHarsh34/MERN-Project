75de70e045847619ae5f53a5267ab10b11916d8d
sidHarsh34 <140314434+sidHarsh34@users.noreply.github.com>
2024-09-14 01:40:13 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 mongoose.connect("mongodb://localhost:27017/mern-auth").then(() =>{
     console.log('Connect to mongodb');
@@ -12,8 +13,11 @@ mongoose.connect("mongodb://localhost:27017/mern-auth").then(() =>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
