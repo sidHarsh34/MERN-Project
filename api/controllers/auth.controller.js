966de3a298986b0b3be1033bc20a135ccdec59b6
sidHarsh34 <140314434+sidHarsh34@users.noreply.github.com>
2024-09-15 02:56:58 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signup = async(req,res,next) => {
     const { username, email, password } = req.body;
@@ -12,5 +15,21 @@ export const signup = async(req,res,next) => {
     }
     catch (error){
         next(error);
+    }
+};
+
+export const signin = async ( req, res, next) => {
+    const { email, password} = req.body;
+    try {
+        const validUser = await User.findOne({ email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'wrong cradentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() = 360000);
+        res.cookie('access_token', token, { httponly: true, expires: expiryDate}).status(200).json(rest);
+    }catch(error){
+            next(error);
     }
 };
